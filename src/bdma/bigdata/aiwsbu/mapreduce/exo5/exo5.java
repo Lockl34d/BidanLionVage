cdcd7f58e7e71a5ca22b7f8cb3226bb292909475
Hadoop BDMA <lion.adam.otman@gmail.com>
2019-01-17 08:27:33 +0100

    exo5 mapreduce OK

---
+++ src/bdma/bigdata/aiwsbu/mapreduce/exo5/exo5.java
@@ -0,0 +1,57 @@
+package bdma.bigdata.aiwsbu.mapreduce.exo5;
+
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+
+
+public class exo5 extends Configured implements Tool {
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new exo5(), args);
+    	System.out.println(exitCode);
+    }
+    
+    
+    public int run(String[] args) throws Exception{
+        /*if (args.length != 2) {
+            System.out.printf("Usage: %s <INPUT> <OUTPUT>\n", getClass().getSimpleName());
+            return -1;
+        }*/
+        //Job job = Job.getInstance();
+    	
+    	Configuration config = HBaseConfiguration.create();
+    	Job job = new Job(config,"Exo5");
+        job.setJarByClass(exo5.class);
+        Scan scan = new Scan();
+        scan.setCaching(500);
+        scan.setCacheBlocks(false);
+        TableName tableName = TableName.valueOf("A:G");
+        String out = "A:CINQ";
+        job.setOutputKeyClass(Text.class); 
+        job.setOutputValueClass(LongWritable.class);
+        TableMapReduceUtil.initTableMapperJob(tableName, scan, exo5Mapper.class, Text.class,
+                Text.class, job);
+        TableMapReduceUtil.initTableReducerJob(out, exo5Reducer.class, job);
+        
+        
+        return job.waitForCompletion(true) ? 0 : 1;
+    	
+    }
+     
+    
+	
+	
+}
---
+++ src/bdma/bigdata/aiwsbu/mapreduce/exo5/exo5Mapper.java
@@ -0,0 +1,70 @@
+package bdma.bigdata.aiwsbu.mapreduce.exo5;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class exo5Mapper extends TableMapper<Text, Text>{
+	HashMap<String,String> semester_to_promo = new HashMap<>();
+	
+	@Override
+	protected void setup(TableMapper<Text, Text>.Context context) throws IOException, InterruptedException {
+		semester_to_promo.put("01", "L1");
+		semester_to_promo.put("02", "L1");
+		semester_to_promo.put("03", "L2");
+		semester_to_promo.put("04", "L2");
+		semester_to_promo.put("05", "L3");
+		semester_to_promo.put("06", "L3");
+		semester_to_promo.put("07", "M1");
+		semester_to_promo.put("08", "M1");
+		semester_to_promo.put("09", "M2");
+		semester_to_promo.put("10", "M2");
+		
+	}
+	
+	
+	@Override
+	protected void map(ImmutableBytesWritable key, Result value,
+			Mapper<ImmutableBytesWritable, Result, Text, Text>.Context context)
+			throws IOException, InterruptedException {
+			//a/s/e/u n
+				String keyStr = key.toString();
+				keyStr = keyStr.replaceAll("\\s", "");
+				keyStr = decode(keyStr);
+				String[] keyPart = keyStr.split("/");
+				
+				Text key_res = new Text( semester_to_promo.get(keyPart[1])+ "/" + keyPart[0]);
+
+				
+				byte[] val = value.getValue(Bytes.toBytes("#"), Bytes.toBytes("G"));
+				String note = new String(val, "UTF-8");
+				
+				
+
+				
+				context.write(key_res, new Text(note));
+				
+				
+				//p/s n
+	}
+
+		public static String decode(String hexString) {
+			int len = hexString.length();
+			if (len%2!=0) {
+				throw new RuntimeException("bad length");
+			}
+			StringBuilder sb = new StringBuilder(len/2);
+			for (int i=0; i<len; i+=2) {
+				final String code = hexString.substring(i, i+2);
+				sb.append((char)Integer.parseInt(code, 16));
+			}
+			return sb.toString();
+		}
+	
+}
